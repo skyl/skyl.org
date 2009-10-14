cb330972c4e340ee4fd95313d4ae1875bae0a524
Skylar Saveland <skylar.saveland@gmail.com>
2009-10-14 06:17:54 -0400

    adding tornado

---
+++ testwsgi.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# Copyright 2009 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import django.core.handlers.wsgi
+import os
+import tornado.httpserver
+import tornado.ioloop
+import tornado.wsgi
+
+def main():
+    os.environ["DJANGO_SETTINGS_MODULE"] = 'settings'
+    application = django.core.handlers.wsgi.WSGIHandler()
+    container = tornado.wsgi.WSGIContainer(application)
+    http_server = tornado.httpserver.HTTPServer(container)
+    http_server.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
